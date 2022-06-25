ff2d6b8285b24cce6e524a7181e8605e95a39144
jisung324 <jisung3245@gmail.com>
2022-06-25 20:21:19 +0900

    vue 문법 스터디

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{logo}}
+    <h4 class="blue" :style='스타일'> 원룸</h4>
+    <p>{{price1}}만원</p>
+  </div>
+    <div>
+    <h4> 원룸</h4>
+    <p>{{price2}}만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      price1 : 50,
+      price2 : 70,
+      logo : '원룸샵',
+      스타일 : 'color : blue',
+    }
+  },
   components: {
-    HelloWorld
-  }
+     }
 }
 </script>
 
