c48477f9af40ca3de64252a4647fe3a64ac1be7a
d483ulert <tottenham3@naver.com>
2022-09-21 22:36:13 +0900

    vue component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,25 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    {{logo}}
-    <h4 class="blue" :style='스타일'> 원룸</h4>
-    <p>{{price1}}만원</p>
-  </div>
-    <div>
-    <h4> 원룸</h4>
-    <p>{{price2}}만원</p>
-  </div>
+
+
 </template>
 
-<script>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js">
 
-export default {
-  name: 'App',
-  data(){
-    return{
-      price1 : 50,
-      price2 : 70,
-      logo : '원룸샵',
-      스타일 : 'color : blue',
-    }
+Vue.component()
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    name: '코지'
   },
-  components: {
-     }
-}
+  methods: {
+    changeText(){
+      this.name='코지';
+    }
+  }
+})
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
