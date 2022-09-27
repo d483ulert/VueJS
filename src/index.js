4a861d9e8736ceae6f111ccba611e3a7eb00e7ab
d483ulert <tottenham3@naver.com>
2022-09-27 22:40:03 +0900

    vuex index.js 추가

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: { //데이터 넣는곳
+
+    },
+    mutations: {
+        //데이터를 실질적으로 바꾸는곳
+    },
+    actions: {
+        //함수 및 비동기적 행위
+    },
+    getters: {
+    }
+
+})
