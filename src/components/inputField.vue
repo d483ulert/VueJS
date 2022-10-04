ade9030ac6d50a1e095848d63d72aa3c017faa87
d483ulert <tottenham3@naver.com>
2022-10-04 21:38:42 +0900

    vue emit 복습

---
+++ src/components/inputField.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <label for="">Name</label>
+  <input
+      type="text"
+      :value="name"
+      @input="$emit('update-name',$event)"
+  >
+
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    }
+  },
+  methods: {
+    updateName(e) {
+      console.log(e.target.value);
+      this.$emit('update-name',e.target.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>This is home Page</h1>
+    <form action="">
+      <inputField
+        :name="name"
+        @update-name="name = $event.target.value"
+      />
+      <button>sumbit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import inputField from  "@/components/inputField";
+
+export default {
+  components: {
+    inputField
+  },
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    updateName(name) {
+      this.name = name;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
