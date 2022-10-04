1b17dccca032e9728d7900378c3c4881ed676a60
d483ulert <tottenham3@naver.com>
2022-10-04 21:13:11 +0900

    vue props 복습

---
+++ src/components/jisung.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ name }}</p>
+    <button
+        @click="updateName">change
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      name: 'jisung'
+    }
+  },
+  methods: {
+    upadateName() {
+      this.name = 'jisung';
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>gg</h1>
+    <jisung
+        :title="title"
+    />
+  </div>
+</template>
+
+<script>
+import jisung from "@/components/jisung";
+
+export default {
+  components: {
+    jisung
+  },
+  data() {
+    return {
+      title: 5
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
